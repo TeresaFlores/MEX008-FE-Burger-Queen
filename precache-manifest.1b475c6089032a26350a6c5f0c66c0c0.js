83111f742f3474c569f281316f35be988f3131e2
TeresaFlores <teresaflores1730@gmail.com>
2019-11-25 18:31:59 -0600

    Updates

---
+++ precache-manifest.1b475c6089032a26350a6c5f0c66c0c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ed5792f914ca29ead6ef53dd30c1e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a2b0d28bc7b2aa0883b",
+    "url": "./static/css/2.4a1d8450.chunk.css"
+  },
+  {
+    "revision": "531f05b772b6df3d5e07",
+    "url": "./static/css/main.447c342b.chunk.css"
+  },
+  {
+    "revision": "8a2b0d28bc7b2aa0883b",
+    "url": "./static/js/2.95723c84.chunk.js"
+  },
+  {
+    "revision": "531f05b772b6df3d5e07",
+    "url": "./static/js/main.84f6b789.chunk.js"
+  },
+  {
+    "revision": "f331254521d27136496c",
+    "url": "./static/js/runtime~main.472a9bfa.js"
+  },
+  {
+    "revision": "3997785d568253ee608815aa1bf747f3",
+    "url": "./static/media/hot-dog.3997785d.png"
+  },
+  {
+    "revision": "25b4db2a32747c4eb716c9126e50369b",
+    "url": "./static/media/lemonade.25b4db2a.png"
+  },
+  {
+    "revision": "0c3813aaf78aa430f8d20db0ae3b8d74",
+    "url": "./static/media/logodemonium.0c3813aa.png"
+  },
+  {
+    "revision": "a5d80e4f5423b7cf7118c9edf4aa6e00",
+    "url": "./static/media/next.a5d80e4f.png"
+  },
+  {
+    "revision": "0a82d1f91a990842118de7c496ead5f9",
+    "url": "./static/media/offer.0a82d1f9.png"
+  },
+  {
+    "revision": "5906a54d5e32cb36405255f4f7576a04",
+    "url": "./static/media/pizzaicon.5906a54d.png"
+  },
+  {
+    "revision": "aa5b3e0301b785645472fc55c7b8774b",
+    "url": "./static/media/salad.aa5b3e03.png"
+  }
+]);
